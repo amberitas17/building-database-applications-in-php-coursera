4338048115348e4b6f3851fb49a7ef4ecdad86fb
amberitas17 <67531380+amberitas17@users.noreply.github.com>
2021-07-09 09:25:37 +0800

    Add files via upload

---
+++ Week 5/app.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Amber Clarisse Cubacub 0816d3a3 - Automobiles Database</title>
+<?php require_once "bootstrap.php"; ?>
+</head>
+<body>
+<div class="container">
+<h1>Amber's Automobiles Database</h1>
+<p>
+<a href="login.php">Please log in</a>
+</p>
+<p>
+Attempt to
+<a href="add.php">add.php</a> without logging in
+</p>
+</div>
+</body>
+</html>
