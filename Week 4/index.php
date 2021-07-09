452b618af8986930f4e5ae51081b7ad027c766da
amberitas17 <67531380+amberitas17@users.noreply.github.com>
2021-07-09 09:23:43 +0800

    Add files via upload

---
+++ Week 4/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Amber Clarisse M. Cubacub e77dc2e0 Login Page </title>
+<?php require_once "bootstrap.php"; ?>
+</head>
+<body>
+<div class="container">
+<h1>Welcome to Autos Database</h1>
+<p><strong>Note:</strong> This sample code is only
+partially done and serves only as a starting point for the assignment.
+</p>
+<p>
+<a href="login.php">Please Log In</a>
+</p>
+<p>
+Attempt to go to 
+<a href="view.php">view.php </a> without logging in - it should fail with an error message.
+</p>
+</div>
+</body>
+
