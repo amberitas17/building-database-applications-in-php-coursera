dc07b33fc279cc28ed3c43b72f18d90e08a76ff6
amberitas17 <67531380+amberitas17@users.noreply.github.com>
2021-07-09 09:33:12 +0800

    Add files via upload

---
+++ Week 5/crud.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 09, 2021 at 03:32 AM
+-- Server version: 10.4.19-MariaDB
+-- PHP Version: 8.0.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `crud`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `autos`
+--
+
+CREATE TABLE `autos` (
+  `autos_id` int(11) NOT NULL,
+  `make` varchar(255) DEFAULT NULL,
+  `model` varchar(255) DEFAULT NULL,
+  `year` int(11) DEFAULT NULL,
+  `mileage` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `autos`
+--
+
+INSERT INTO `autos` (`autos_id`, `make`, `model`, `year`, `mileage`) VALUES
+(3, 'Mitsubishi', 'Innova', 2018, 1222),
+(4, 'Audi', 'RS 5 Coupe', 2004, 1234),
+(5, 'Toyota', 'Corolla', 2000, 1000);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `autos`
+--
+ALTER TABLE `autos`
+  ADD PRIMARY KEY (`autos_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `autos`
+--
+ALTER TABLE `autos`
+  MODIFY `autos_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
